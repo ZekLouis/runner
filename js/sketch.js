bd2ca8376b76596a9ab5ad4958c96a2c204bb096
Louis Gaume <louis.gaume@etu.unilim.fr>
2016-12-07 18:56:10 +0100

    aze

--- js/sketch.js
+++ js/sketch.js
@@ -1,70 +1,17 @@
-/*function setup() {
-  createCanvas($(window).width(), $(window).height());
-}
-
-var xpos = 100;
- 
-// y position variable
-var ground = 500-(25/2)
-var ypos = ground;
- 
-function draw(){
-  // clear background
-  background(255);
-  text('Runner 1 min de trajet lol', 25, 25);
- 
-  // set the fill color
-  fill(0, 102, 204);
- 
-  // black outline
-  stroke(0);
- 
-  // set the ellipse mode
-  ellipseMode(CENTER);
- 
-  // draw a circle
-  ellipse(xpos, ypos, 25, 25);
-
-  fill(102, 51, 0);
-  rect(0, 500, $(window).width(), 500);
-
-  if (keyIsDown(LEFT_ARROW) && xpos > 0)
-    xpos-=5;
-
-  if (keyIsDown(RIGHT_ARROW) && xpos < $(window).width())
-    xpos+=5;
-
-	// if (keyIsDown(32) && ypos > 300){
-	// 	ypos -= 5
- //    	setTimeout(function(){
- //    		if(ypos < ground)
-	// 	    	ypos += 5
-	// 	}, 200);
-	// }
-}
- 
-function keyPressed(){
-	if (keyCode == 32 && ypos > 300){
-		ypos -= 50
-    	setTimeout(function(){
-    		if(ypos < ground)
-		    	ypos += 50
-		}, 200);
-	}
-	console.log(xpos,ypos)
-}*/
+//TODO Gérer collision latérale plateform
 
 //NEW PROJECT PLAY
 
 var xpos = 400;
 var ground = 500-(25/2)
 var ypos = 500;
-var GROUND_Y = $(window).height()-50;
+var GROUND_Y = ($(window).height()-50);
 var speed = 0.5;
 var jump = 30;
 var marge_joueur_ecran = 400;
 var jumping = false;
 var gagne = false;
+var score = 0;
 
 function setup(){
 	createCanvas($(window).width(), $(window).height());
@@ -98,13 +45,12 @@ function setup(){
 }
 
 function draw(){
-	var i = 0;
-	i++;
 	background(200);
-	text('Runner 1 min de trajet lol'+i, 25, 25);
+	text('Runner 1 min de trajet lol', 25, 25);
 	text('Arrow, Backspace and R', 25, 50);
+	text(score, 25, 75)
 	drawSprites();
-	console.log(platform2.position.x);
+	//console.log(platform2.position.x);
 
 	if (keyIsDown(LEFT_ARROW) && xpos > 0 && pers.visible == true){
 		platform2.position.x += 5
@@ -114,6 +60,7 @@ function draw(){
 
 	if (keyIsDown(RIGHT_ARROW) && xpos < $(window).width() && pers.visible == true){
 		platform2.position.x -= 5
+		score += 10
 		for(var i = 0; i<obstaclesSp.length; i++)
 			obstaclesSp[i].position.x -= 5
 	}
@@ -161,8 +108,9 @@ function keyPressed(){
 	if (keyCode == 82){
 		pers.visible = true
 		pers.position.x = xpos
-		pers.position.y = ypos
+		pers.position.y = GROUND_Y
 		pers.velocity.y = 7
+		score = 0
 		for(var i = 0; i<obstacles.length; i++){
 			obstacles[i].resetPos();
 		}
