7cad10552a0c168e1b0f6c45aca3781c7e87c4aa
Louis Gaume <louis.gaume@etu.unilim.fr>
2016-12-12 12:39:42 +0100

    json

--- php/postScore.php
+++ php/postScore.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET['pseudo']) && isset($_GET['score'] && $_GET['num_requete']==1)){
+if(isset($_GET['pseudo']) && isset($_GET['score']) && $_GET['num_requete']==1){
     include_once('config.inc.php');
     include_once('Mypdo.class.php');
     include_once('Score.class.php');
@@ -7,7 +7,7 @@ if(isset($_GET['pseudo']) && isset($_GET['score'] && $_GET['num_requete']==1)){
 
     if($_GET['pseudo'] == preg_replace("/[^a-zA-Z0-9 ]+/", "", $_GET['pseudo'])){
 
-        if(strlen(trim($_GET['pseudo'])) == 0 || $_PST['pseudo'] == "undefined")
+        if(strlen(trim($_GET['pseudo'])) == 0 || $_GET['pseudo'] == "undefined")
             $_GET['pseudo'] = "Nobody";
             
         $db = new Mypdo();
