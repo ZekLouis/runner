37dea5c1e5bba2217ff4bd17196f79e1428de14e
Louis Gaume <louis.gaume@etu.unilim.fr>
2016-12-12 12:41:59 +0100

    json

--- php/ScoreManager.class.php
+++ php/ScoreManager.class.php
@@ -8,8 +8,8 @@ class ScoreManager{
 
 	public function addScore($score){
 		$req = $this->db->prepare('INSERT INTO score(pseudo, score) values (:pseudo, :score);');
-		$req->bindValue(':pseudo',$score->getPseudo(),PDO::PARAM_STR);
-        $req->bindValue(':score',$score->getScore(),PDO::PARAM_STR);
+		$req->bindValue(':pseudo',$score->getPseudo());
+        $req->bindValue(':score',$score->getScore());
 		$req->execute();
 	}
 }
